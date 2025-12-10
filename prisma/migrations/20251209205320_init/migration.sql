5c4717e4dced9dc33e0f2ce585cc50688c7033ee
Rafael Ribas de Lima <ribas7494@gmail.com>
2025-12-10 19:03:18 -0300

    Padrão Pluggy

--- prisma/migrations/20251209205320_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "AnaliseFinanceira" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "itemId" TEXT NOT NULL,
-    "perfil" TEXT NOT NULL,
-    "renda" REAL NOT NULL,
-    "gastos" REAL NOT NULL,
-    "data" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
